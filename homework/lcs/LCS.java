6725ec5d08af8c32d310f18d028d67331465875c
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-03-27 21:24:52 -0700

    Add support of BU and find solution

--- homework/lcs/LCS.java
+++ homework/lcs/LCS.java
@@ -1,5 +1,7 @@
 package lcs;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class LCS {
@@ -15,8 +17,30 @@ public class LCS {
     // Shared Helper Methods
     // -----------------------------------------------
     
-    Set<String> collectSolution (String rStr, int r, String cStr, int c, int[][] memo) {
-    	throw new UnsupportedOperationException();
+    private static Set<String> collectSolution (String rStr, int r, String cStr, int c, int[][] memo) {
+    	if (r == 0 || c == 0) {
+    		return new HashSet<String>(Arrays.asList(""));
+    	}
+    	if (rStr.charAt(r - 1) == cStr.charAt(c - 1)) {
+    		Set<String> continuing = collectSolution(rStr, r - 1, cStr, c - 1, memo);
+    		Set<String> result = new HashSet<String>();
+    		for (String s : continuing) {
+    			result.add(s + rStr.charAt(r - 1));
+    		}
+    		return result;
+    	}
+    	Set<String> result = new HashSet<String>();
+    	if (memo[r][c - 1] >= memo[c][r - 1]) {
+    		result.addAll(collectSolution(rStr, r, cStr, c - 1, memo));
+    	}
+    	if (memo[r - 1][c] >= memo[c - 1][r]){
+    		result.addAll(collectSolution(rStr, r - 1, cStr, c, memo));
+    	}
+    	return result;
+    }
+    
+    private static boolean checkMatchedLetters(int index1, int index2, String rStr, String cStr) {
+    	return rStr.charAt(index1) == cStr.charAt(index2);
     }
     
 
@@ -34,7 +58,17 @@ public class LCS {
      *         [Side Effect] sets memoCheck to refer to table
      */
     public static Set<String> bottomUpLCS (String rStr, String cStr) {
-        throw new UnsupportedOperationException();
+        memoCheck = new int[rStr.length() + 1][cStr.length() + 1];
+        for (int i = 1; i < rStr.length() + 1; i++) {
+        	for (int j = 1; j < cStr.length() + 1; j++) {
+        		if (checkMatchedLetters(i - 1, j - 1, rStr, cStr)) {
+                	memoCheck[i][j] = memoCheck[i - 1][j - 1] + 1;
+                } else {
+                	memoCheck[i][j] = Math.max(memoCheck[i - 1][j], memoCheck[i][j - 1]);
+                }
+        	}
+        }
+        return collectSolution(rStr, rStr.length(), cStr, cStr.length(), memoCheck);
     }
     
     // [!] TODO: Add any bottom-up specific helpers here!
