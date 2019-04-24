a3ef2853669eb10ffa47ad15d0c9a326aee37374
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-04-24 08:35:47 -0700

    Add solution tests

---
+++ homework/lcs/LCSSolutionTests.java
@@ -0,0 +1,690 @@
+package lcs;
+
+import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.rules.Timeout;
+import org.junit.runner.Description;
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class LCSSolutionTests {
+    
+    // =================================================
+    // Test Configuration
+    // =================================================
+    
+    // Global timeout to prevent infinite loops from
+    // crashing the test suite + to test that your
+    // memoization is working...
+    // If they are, 3 seconds should be more than enough
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(3);
+    
+    // Grade record-keeping
+    static int possible = 0, passed = 0;
+    
+    // the @Before method is run before every @Test
+    @Before
+    public void init () {
+        possible++;
+    }
+    
+    // Each time you pass a test, you get a point! Yay!
+    // [!] Requires JUnit 4+ to run
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+            passed++;
+        }
+    };
+    
+    // Used for grading, reports the total number of tests
+    // passed over the total possible
+    @AfterClass
+    public static void gradeReport () {
+        System.out.println("============================");
+        System.out.println("Tests Complete");
+        System.out.println(passed + " / " + possible + " passed!");
+        if ((1.0 * passed / possible) >= 0.9) {
+            System.out.println("[!] Nice job!"); // Automated acclaim!
+        }
+        System.out.println("============================");
+    }
+    
+    
+    // =================================================
+    // Unit Tests
+    // =================================================
+    
+    // Bottom-up LCS Tests
+    // -----------------------------------------------
+    @Test
+    public void BULCSTest_t0() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                ""
+            )),
+            LCS.bottomUpLCS("", "")
+        );
+        // LCS.memoCheck can either be the 1 element matrix
+        // or null -- up to you, I won't check for cases with
+        // empty-String arguments
+    }
+    
+    @Test
+    public void BULCSTest_t1() {
+        // First assertion: correctness test for set with LCS
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                ""
+            )),
+            LCS.bottomUpLCS("A", "B")
+        );
+        // Second assertion: proper format / solution of memo table
+        assertArrayEquals(
+            new int[][] {
+                {0, 0},
+                {0, 0}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t2() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "A"
+            )),
+            LCS.bottomUpLCS("A", "A")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0},
+                {0, 1}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t3() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABC"
+            )),
+            LCS.bottomUpLCS("ABC", "ABC")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 1, 1, 1},
+                {0, 1, 2, 2},
+                {0, 1, 2, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t4() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "AA", "BA"
+            )),
+            LCS.bottomUpLCS("ABA", "BAA")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 1, 1},
+                {0, 1, 1, 1},
+                {0, 1, 2, 2}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t5() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "A", "B", "C"
+            )),
+            LCS.bottomUpLCS("ABC", "CBA")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 1},
+                {0, 1, 1, 1}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t6() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                ""
+            )),
+            LCS.bottomUpLCS("ABC", "DEF")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t7() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "A", "B", "C"
+            )),
+            LCS.bottomUpLCS("ABCC", "CBA")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 1},
+                {0, 1, 1, 1},
+                {0, 1, 1, 1}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t8() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABC", "XBC", "XBZ", "ABZ"
+            )),
+            LCS.bottomUpLCS("XABZC", "AXBCZ")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1},
+                {0, 1, 1, 1, 1, 1},
+                {0, 1, 1, 2, 2, 2},
+                {0, 1, 1, 2, 2, 3},
+                {0, 1, 1, 2, 3, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t9() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABC", "XBC"
+            )),
+            LCS.bottomUpLCS("XABZC", "AXBC")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1},
+                {0, 1, 1, 1, 1},
+                {0, 1, 1, 2, 2},
+                {0, 1, 1, 2, 2},
+                {0, 1, 1, 2, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t10() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCDE"
+            )),
+            LCS.bottomUpLCS("XABCDEX", "YABCDEY")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 1},
+                {0, 0, 1, 2, 2, 2, 2, 2},
+                {0, 0, 1, 2, 3, 3, 3, 3},
+                {0, 0, 1, 2, 3, 4, 4, 4},
+                {0, 0, 1, 2, 3, 4, 5, 5},
+                {0, 0, 1, 2, 3, 4, 5, 5}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t11() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCXEZ"
+            )),
+            LCS.bottomUpLCS("AVBWCXDYEZ", "ABYCXWEZDV")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+                {0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5},
+                {0, 1, 2, 3, 3, 4, 4, 4, 4, 5, 5},
+                {0, 1, 2, 3, 3, 4, 4, 5, 5, 5, 5},
+                {0, 1, 2, 3, 3, 4, 4, 5, 6, 6, 6}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t12() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCXEZ"
+            )),
+            LCS.bottomUpLCS("AVBWCXDEZ", "ABYCXWEZDV")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+                {0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5},
+                {0, 1, 2, 2, 3, 4, 4, 5, 5, 5, 5},
+                {0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 6}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t13() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCYZ", "ABCDE", "VWXYZ", "VWXDE"
+            )),
+            LCS.bottomUpLCS("ABCVWXDEYZ", "VWXABCYZDE")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+                {0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2},
+                {0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 3},
+                {0, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3},
+                {0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3},
+                {0, 1, 2, 3, 3, 3, 3, 3, 3, 4, 4},
+                {0, 1, 2, 3, 3, 3, 3, 3, 3, 4, 5},
+                {0, 1, 2, 3, 3, 3, 3, 4, 4, 4, 5},
+                {0, 1, 2, 3, 3, 3, 3, 4, 5, 5, 5}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void BULCSTest_t14() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABW", "YZX", "RSX", "YZW", "RSW", "ABX"
+            )),
+            LCS.bottomUpLCS("ABRSYZXW", "YZRSABWX")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 1, 1, 1},
+                {0, 0, 0, 0, 0, 1, 2, 2, 2},
+                {0, 0, 0, 1, 1, 1, 2, 2, 2},
+                {0, 0, 0, 1, 2, 2, 2, 2, 2},
+                {0, 1, 1, 1, 2, 2, 2, 2, 2},
+                {0, 1, 2, 2, 2, 2, 2, 2, 2},
+                {0, 1, 2, 2, 2, 2, 2, 2, 3},
+                {0, 1, 2, 2, 2, 2, 2, 3, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    
+    // Top-Down LCS Tests
+    // -----------------------------------------------
+    @Test
+    public void TDLCSTest_t0() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                ""
+            )),
+            LCS.topDownLCS("", "")
+        );
+        // LCS.memoCheck can either be the 1 element matrix
+        // or null -- up to you, I won't check for cases with
+        // empty-String arguments
+    }
+    
+    @Test
+    public void TDLCSTest_t1() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                ""
+            )),
+            LCS.topDownLCS("A", "B")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0},
+                {0, 0}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t2() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "A"
+            )),
+            LCS.topDownLCS("A", "A")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0},
+                {0, 1}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t3() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABC"
+            )),
+            LCS.topDownLCS("ABC", "ABC")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 2, 0},
+                {0, 0, 0, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t4() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "AA", "BA"
+            )),
+            LCS.topDownLCS("ABA", "BAA")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 1, 1, 0},
+                {0, 0, 0, 2}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t5() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "A", "B", "C"
+            )),
+            LCS.topDownLCS("ABC", "CBA")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 1},
+                {0, 1, 1, 1}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t6() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                ""
+            )),
+            LCS.topDownLCS("ABC", "DEF")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t7() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "A", "B", "C"
+            )),
+            LCS.topDownLCS("ABCC", "CBA")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 1},
+                {0, 1, 1, 1},
+                {0, 1, 1, 1}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t8() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABC", "XBC", "XBZ", "ABZ"
+            )),
+            LCS.topDownLCS("XABZC", "AXBCZ")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 0},
+                {0, 1, 1, 2, 2, 0},
+                {0, 1, 1, 2, 0, 3},
+                {0, 0, 0, 0, 3, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t9() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABC", "XBC"
+            )),
+            LCS.topDownLCS("XABZC", "AXBC")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 1, 1, 2, 0},
+                {0, 1, 1, 2, 0},
+                {0, 0, 0, 0, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t10() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCDE"
+            )),
+            LCS.topDownLCS("XABCDEX", "YABCDEY")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 1, 1, 1, 1},
+                {0, 0, 1, 2, 2, 2, 2, 2},
+                {0, 0, 1, 2, 3, 3, 3, 3},
+                {0, 0, 1, 2, 3, 4, 4, 4},
+                {0, 0, 1, 2, 3, 4, 5, 5},
+                {0, 0, 1, 2, 3, 4, 5, 5}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t11() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCXEZ"
+            )),
+            LCS.topDownLCS("AVBWCXDYEZ", "ABYCXWEZDV")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+                {0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5},
+                {0, 0, 0, 3, 3, 4, 4, 4, 4, 5, 5},
+                {0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5},
+                {0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t12() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCXEZ"
+            )),
+            LCS.topDownLCS("AVBWCXDEZ", "ABYCXWEZDV")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2},
+                {0, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {0, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 4, 4},
+                {0, 1, 2, 2, 3, 4, 4, 4, 4, 5, 5},
+                {0, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5},
+                {0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t13() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABCYZ", "ABCDE", "VWXYZ", "VWXDE"
+            )),
+            LCS.topDownLCS("ABCVWXDEYZ", "VWXABCYZDE")
+        );
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 1, 2, 2, 2, 2, 0, 0},
+                {0, 0, 0, 0, 1, 2, 3, 3, 3, 0, 0},
+                {0, 1, 1, 1, 1, 2, 3, 3, 3, 0, 0},
+                {0, 1, 2, 2, 2, 2, 3, 3, 3, 0, 0},
+                {0, 1, 2, 3, 3, 3, 3, 3, 3, 0, 0},
+                {0, 1, 2, 3, 3, 3, 3, 3, 3, 4, 0},
+                {0, 1, 2, 3, 3, 3, 3, 3, 3, 4, 5},
+                {0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 5},
+                {0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 5}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+    @Test
+    public void TDLCSTest_t14() {
+        assertEquals(
+            new HashSet<>(Arrays.asList(
+                "ABW", "YZX", "RSX", "YZW", "RSW", "ABX"
+            )),
+            LCS.topDownLCS("ABRSYZXW", "YZRSABWX")
+        );
+        
+        assertArrayEquals(
+            new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 1, 1, 0},
+                {0, 0, 0, 0, 0, 1, 2, 2, 0},
+                {0, 0, 0, 1, 1, 1, 2, 2, 0},
+                {0, 0, 0, 1, 2, 2, 2, 2, 0},
+                {0, 1, 1, 1, 2, 2, 2, 2, 0},
+                {0, 1, 2, 2, 2, 2, 2, 2, 0},
+                {0, 1, 2, 2, 2, 2, 2, 0, 3},
+                {0, 0, 0, 0, 0, 0, 0, 3, 3}
+            },
+            LCS.memoCheck
+        );
+    }
+    
+}
---
+++ homework/nim/NimSolutionTests.java
@@ -0,0 +1,248 @@
+package nim;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.rules.Timeout;
+import org.junit.runner.Description;
+
+public class NimSolutionTests {
+    
+    // =================================================
+    // Test Configuration
+    // =================================================
+    
+    // Global timeout to prevent infinite loops from
+    // crashing the test suite + to test that your
+    // alpha-beta pruning and memoization are working;
+    // If they are, 3 seconds should be more than enough
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(3);
+    
+    // Grade record-keeping
+    static int possible = 0, passed = 0;
+    
+    // the @Before method is run before every @Test
+    @Before
+    public void init () {
+        possible++;
+    }
+    
+    // Each time you pass a test, you get a point! Yay!
+    // [!] Requires JUnit 4+ to run
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        protected void succeeded(Description description) {
+            passed++;
+        }
+    };
+    
+    // Used for grading, reports the total number of tests
+    // passed over the total possible
+    @AfterClass
+    public static void gradeReport () {
+        System.out.println("============================");
+        System.out.println("Tests Complete");
+        System.out.println(passed + " / " + possible + " passed!");
+        if ((1.0 * passed / possible) >= 0.9) {
+            System.out.println("[!] Nice job!"); // Automated acclaim!
+        }
+        System.out.println("============================");
+    }
+    
+    // =================================================
+    // Unit Tests
+    // =================================================
+    
+    
+    /**
+     * Basic test to make sure the nimesis knows the
+     * base cases: how to win when presented with the
+     * opportunity
+     */
+    @Test
+    public void NimPlayerTest_t0() {
+        NimPlayer nimesis = new NimPlayer(2);
+        assertEquals(1, nimesis.choose(1));
+        assertEquals(2, nimesis.choose(2));
+    }
+    
+    /**
+     * One-ply above winning condition to see it
+     * the nimesis can put itself into a sure win
+     */
+    @Test
+    public void NimPlayerTest_t1() {
+        NimPlayer nimesis = new NimPlayer(2);
+        assertEquals(1, nimesis.choose(4));
+        assertEquals(2, nimesis.choose(5));
+    }
+    
+    /**
+     * OK, two-ply now, just to make sure!
+     */
+    @Test
+    public void NimPlayerTest_t2() {
+        NimPlayer nimesis = new NimPlayer(2);
+        assertEquals(1, nimesis.choose(6));
+        assertEquals(1, nimesis.choose(7));
+    }
+    
+    /**
+     * OK, can now take between 1 - 3 stones
+     */
+    @Test
+    public void NimPlayerTest_t3() {
+        NimPlayer nimesis = new NimPlayer(3);
+        assertEquals(1, nimesis.choose(1));
+        assertEquals(2, nimesis.choose(2));
+        assertEquals(3, nimesis.choose(3));
+    }
+    
+    /**
+     * Same, but 1-ply from victory
+     */
+    @Test
+    public void NimPlayerTest_t4() {
+        NimPlayer nimesis = new NimPlayer(3);
+        assertEquals(1, nimesis.choose(5));
+        assertEquals(2, nimesis.choose(6));
+        assertEquals(3, nimesis.choose(7));
+    }
+    
+    /**
+     * Same, but 2-ply from victory
+     */
+    @Test
+    public void NimPlayerTest_t5() {
+        NimPlayer nimesis = new NimPlayer(3);
+        assertEquals(2, nimesis.choose(10));
+        assertEquals(1, nimesis.choose(9));
+        assertEquals(1, nimesis.choose(8));
+    }
+    
+    /**
+     * OK, welcome to flavor country
+     */
+    @Test
+    public void NimPlayerTest_t6() {
+        NimPlayer nimesis = new NimPlayer(3);
+        assertEquals(1, nimesis.choose(40));
+        assertEquals(3, nimesis.choose(39));
+        assertEquals(2, nimesis.choose(38));
+    }
+    
+    /**
+     * OK, *REALLY* welcome to flavor country
+     */
+    @Test
+    public void NimPlayerTest_t7() {
+        NimPlayer nimesis = new NimPlayer(3);
+        assertEquals(1, nimesis.choose(1000));
+    }
+    
+    @Test
+    public void NimPlayerTest_t8() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(1));
+        assertEquals(2, nimesis.choose(2));
+        assertEquals(3, nimesis.choose(3));
+        assertEquals(4, nimesis.choose(4));
+    }
+    
+    @Test
+    public void NimPlayerTest_t9() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(6));
+        assertEquals(2, nimesis.choose(7));
+        assertEquals(3, nimesis.choose(8));
+        assertEquals(4, nimesis.choose(9));
+    }
+    
+    @Test
+    public void NimPlayerTest_t10() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(10));
+        assertEquals(1, nimesis.choose(11));
+        assertEquals(2, nimesis.choose(12));
+        assertEquals(3, nimesis.choose(13));
+        assertEquals(4, nimesis.choose(14));
+    }
+    
+    @Test
+    public void NimPlayerTest_t11() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(11));
+        assertEquals(2, nimesis.choose(12));
+        assertEquals(1, nimesis.choose(10));
+        assertEquals(1, nimesis.choose(10));
+        assertEquals(3, nimesis.choose(13));
+        assertEquals(4, nimesis.choose(14));
+        assertEquals(3, nimesis.choose(13));
+    }
+    
+    @Test
+    public void NimPlayerTest_t12() {
+        NimPlayer nimesis = new NimPlayer(5);
+        assertEquals(1, nimesis.choose(1));
+        assertEquals(2, nimesis.choose(2));
+        assertEquals(3, nimesis.choose(3));
+        assertEquals(4, nimesis.choose(4));
+        assertEquals(5, nimesis.choose(5));
+    }
+    
+    @Test
+    public void NimPlayerTest_t13() {
+        NimPlayer nim1 = new NimPlayer(3);
+        NimPlayer nim2 = new NimPlayer(3);
+        assertEquals(1, nim1.choose(9));
+        assertEquals(3, nim2.choose(7));
+        assertEquals(1, nim1.choose(4));
+        assertEquals(3, nim2.choose(3));
+    }
+    
+    @Test
+    public void NimPlayerTest_t14() {
+        NimPlayer nim1 = new NimPlayer(3);
+        NimPlayer nim2 = new NimPlayer(3);
+        assertEquals(1, nim1.choose(9));
+        assertEquals(1, nim1.choose(9));
+        assertEquals(3, nim2.choose(7));
+        assertEquals(1, nim1.choose(4));
+        assertEquals(1, nim1.choose(4));
+        assertEquals(3, nim2.choose(3));
+        assertEquals(3, nim2.choose(3));
+    }
+    
+    @Test
+    public void NimPlayerTest_t15() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(1000));
+    }
+    
+    @Test
+    public void NimPlayerTest_t16() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(1001));
+        assertEquals(2, nimesis.choose(1002));
+        assertEquals(3, nimesis.choose(1003));
+        assertEquals(4, nimesis.choose(1004));
+    }
+    
+    @Test
+    public void NimPlayerTest_t17() {
+        NimPlayer nimesis = new NimPlayer(4);
+        assertEquals(1, nimesis.choose(1001));
+        assertEquals(2, nimesis.choose(1002));
+        assertEquals(3, nimesis.choose(1003));
+        assertEquals(1, nimesis.choose(1001));
+        assertEquals(3, nimesis.choose(1003));
+        assertEquals(4, nimesis.choose(1004));
+    }
+    
+}
